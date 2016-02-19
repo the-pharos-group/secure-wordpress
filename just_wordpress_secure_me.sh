f8d8306488276cd8088577b2ef99c7a5b20b4e6a
J C <james@jamescampbell.us>
2016-02-19 15:40:47 -0500

    Update just_wordpress_secure_me.sh

--- just_wordpress_secure_me.sh
+++ just_wordpress_secure_me.sh
@@ -1,9 +1,10 @@
-# coming soon the entire bash script to do it all run as sudo ./just_wordpress_secure_me.sh
 #!/bin/bash
 # dont forget to set all of these and make the passwords long
-WPDBNAME="your wordpress database name"
-WPDBUSERNAME="your wordpress database username"
-WPDBUSERPW="your wordpress database user password"
+WPDBNAME="poo"
+WPDBUSERNAME="poo"
+WPDBUSERPW="!1newmedia"
+DOMAINNAME="cooldomain.com"
+WWWDOMAINNAME="www.cooldomain.com"
 echo "Your worpdress databasename is: $WPDBNAME"
 
 # install auto-upgrades for ubuntu so you dont need to worry about it
@@ -11,9 +12,13 @@ sudo apt-get update -y
 sudo apt-get install unattended-upgrades -y
 sudo dpkg-reconfigure unattended-upgrades
 
+# add new user
+
 # install nginx & lock it down
 sudo apt-get install nginx nginx-extras -y
+# turn server tokens off for nginx (shuts off header info)
 sed -i "21i server_tokens off;" /etc/nginx/nginx.conf
+# set headers to a fake Server name to mess with people trying to scan for exploits
 sed -i "22i more_set_headers 'Server: FUCK_OFF_KIDDIES';" /etc/nginx/nginx.conf
 #sudo echo "server_tokens off;" >> /etc/nginx/nginx.conf
 #sudo echo "more_set_headers 'Server: FUCK_OFF_KIDDIES';" >> /etc/nginx/nginx.conf
@@ -21,5 +26,52 @@ sudo nginx -s reload
 
 # install mysql and lock it down
 sudo apt-get install mysql-server
+# dont forget your root password for mysql
 sudo mysql_install_db
 sudo mysql_secure_installation
+
+echo $(mysql -uroot -e 'create database $WPDBNAME;' -p)
+echo $(mysql -uroot -e "create user `$WPDBUSERNAME`@`localhost` IDENTIFIED BY '$WPDBUSERPW';")
+echo $(mysql -uroot -e "grant all privileges on `$WPDBNAME`.* to `$WPDBUSERNAME`@`localhost`;FLUSH PRIVILEGES;")
+
+# get wordpress setup
+cd ~
+mkdir projects && cd projects
+wget http://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz # creates directory called wordpress with all necessary files
+sudo apt-get install php5-gd libssh2-php -y
+
+# clone this repo
+sudo apt-get install git -y
+git clone https://github.com/the-pharos-group/secure-wordpress.git
+
+# move the custom wordpress config to the previously downloaded and tarred wordpress folder
+cp secure-wordpress/wp-config.php wordpress/
+
+# mod the config file with the updated settings
+cd wordpress
+sed -i "20i define( 'DB_NAME',     '$WPDBNAME' );" wp-config.php
+sed -i "21i define( 'DB_USER',     '$WPDBUSERNAME' );" wp-config.php
+sed -i "22i define( 'DB_PASSWORD',     '$WPDBUSERPW' );" wp-config.php
+sed -i "23i define( 'DB_NAME',     '$WPDBNAME' );" wp-config.php
+
+# mod the nginx default from the repo with updated variables from above and move into place
+cd ~/projects/secure-wordpress
+sed -i "3i server_name $DOMAINNAME;" default
+sed -i "4i 301 redirect http://$WWWDOMAINNAME\$request_uri;" default
+sed -i "12i server_name $WWWDOMAINNAME;"
+cp default /etc/nginx/sites-available/default
+
+#restart nginx to make sure everything is working
+sudo service nginx restart
+sudo service php5-fpm restart
+
+# move wordpress code to proper place for nginx default virtual web server path
+sudo mkdir -p /var/www/html
+sudo rsync -avP ~/projects/wordpress/ /var/www/html/
+cd /var/www/html/
+sudo chown -R $USER:www-data /var/www/html/*
+mkdir wp-content/uploads
+
+echo "ALL DONE! GO TO THE IP ADDRESS OF THIS SERVER IN BROWSER AND FINISH INSTALL"
+echo "DONT FORGET TO RUN THE SERVER LOCKDOWN SCRIPT AS WELL TO REALLY SECURE THIS BOX"
